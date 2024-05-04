d97a437fa0c6d3a1662ea3f5bcd47c46bfac627d
makoto <88492711+hihdrum@users.noreply.github.com>
2024-05-04 15:56:31 +0900

    読み込んだジャーナルヘッダから年と月の情報を取得するまで。

--- erlang/jnl.erl
+++ erlang/jnl.erl
@@ -10,3 +10,6 @@ read_open(FileName) -> file:open(FileName, [read, binary, raw]).
 sample_read_open() -> read_open(sample_file()).
 
 read_header(IoDevice) -> file:read(IoDevice, 26).
+
+parse_header(<<Year:4/binary, Month:2/binary, Rest/binary>>) ->
+  {{year, Year}, {month, Month}, {rest, Rest}}.
