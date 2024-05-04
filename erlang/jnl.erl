132094f49522f4553d5071db7aef4d444eef2834
makoto <88492711+hihdrum@users.noreply.github.com>
2024-05-04 17:22:52 +0900

    ジャーナルヘッダ部分の情報を出力するまで。

--- erlang/jnl.erl
+++ erlang/jnl.erl
@@ -11,5 +11,25 @@ sample_read_open() -> read_open(sample_file()).
 
 read_header(IoDevice) -> file:read(IoDevice, 26).
 
-parse_header(<<Year:4/binary, Month:2/binary, Rest/binary>>) ->
-  {{year, Year}, {month, Month}, {rest, Rest}}.
+parse_header(<<Year:4/binary, Month:2/binary, Day:2/binary,
+               Hour:2/binary, Minute:2/binary, Second:2/binary, MSecond:3/binary,
+               Kind:1/binary, DataLen:8/binary>>) ->
+  {{year, Year}, {month, Month}, {day, Day},
+   {hour, Hour}, {minute, Minute}, {second, Second}, {msecond, MSecond},
+   {kind, Kind}, {data_len, DataLen}}.
+
+print_header(Header) ->
+  {{year, Year}, {month, Month}, {day, Day},
+   {hour, Hour}, {minute, Minute}, {second, Second}, {msecond, MSecond},
+   {kind, Kind}, {data_len, DataLen}} = parse_header(Header),
+
+  io:format("~s/~s/~s,~s:~s:~s.~s,~s,~s", [Year, Month, Day, Hour, Minute, Second, MSecond, Kind, DataLen]).
+
+sample_run() ->
+  SampleFile = sample_file(),
+  {ok, IoDevice} = read_open(SampleFile),
+  {ok, HeaderBin} = read_header(IoDevice),
+  Header = parse_header(HeaderBin),
+  print_header(HeaderBin),
+  file:close(IoDevice),
+  Header.
