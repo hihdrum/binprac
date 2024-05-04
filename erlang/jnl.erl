a60e2cab80aa4e494c3cfaa6bec2a9f038bfaa91
makoto <88492711+hihdrum@users.noreply.github.com>
2024-05-04 18:11:56 +0900

    ジャーナルヘッダの表現にレコードを利用するようにした。 1ジャーナルレコードを読み込むまで。

--- erlang/jnl.erl
+++ erlang/jnl.erl
@@ -1,5 +1,6 @@
 -module(jnl).
 -compile(export_all).
+-include("jnl.hrl").
 
 hello() -> "Hello, world!".
 
@@ -25,11 +26,36 @@ print_header(Header) ->
 
   io:format("~s/~s/~s,~s:~s:~s.~s,~s,~s", [Year, Month, Day, Hour, Minute, Second, MSecond, Kind, DataLen]).
 
+parse_jnl_header(HeaderBin) ->
+  <<Year:4/binary, Month:2/binary, Day:2/binary,
+    Hour:2/binary, Minute:2/binary, Second:2/binary, MSecond:3/binary,
+    Kind:1/binary, DataLen:8/binary>> = HeaderBin,
+
+  #jnl_header{
+     year = Year, month = Month, day = Day,
+     hour = Hour, minute = Minute, second = Second, msecond = MSecond,
+     kind = Kind, data_len = DataLen
+    }.
+
+print_jnl_header(HeaderRecord) ->
+  #jnl_header{
+     year = Year, month = Month, day = Day,
+     hour = Hour, minute = Minute, second = Second, msecond = MSecond,
+     kind = Kind, data_len = DataLen} = HeaderRecord,
+
+  io:format("HEADER,~s/~s/~s,~s:~s:~s.~s,~s,~s~n",
+            [Year, Month, Day, Hour, Minute, Second, MSecond, Kind, DataLen]).
+
+read_data(HeaderRecord, IoDevice) ->
+  DataLen = binary_to_integer(HeaderRecord#jnl_header.data_len),
+  file:read(IoDevice, DataLen).
+
 sample_run() ->
   SampleFile = sample_file(),
   {ok, IoDevice} = read_open(SampleFile),
   {ok, HeaderBin} = read_header(IoDevice),
-  Header = parse_header(HeaderBin),
-  print_header(HeaderBin),
+  HeaderRecord = parse_jnl_header(HeaderBin),
+  print_jnl_header(HeaderRecord),
+  {ok, DataBin} = read_data(HeaderRecord, IoDevice),
   file:close(IoDevice),
-  Header.
+  {HeaderRecord, DataBin}.
---
+++ erlang/jnl.hrl
@@ -0,0 +1,11 @@
+-record(jnl_header, {
+          year,
+          month,
+          day,
+          hour,
+          minute,
+          second,
+          msecond,
+          kind,
+          data_len
+         }).
