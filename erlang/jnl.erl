700d3eca82a133c7ccd7e5e80e9bff7c1659c2cd
makoto <88492711+hihdrum@users.noreply.github.com>
2024-05-05 02:20:59 +0900

    書込み処理を修正した。

--- erlang/jnl.erl
+++ erlang/jnl.erl
@@ -24,7 +24,8 @@ print_header(Header) ->
    {hour, Hour}, {minute, Minute}, {second, Second}, {msecond, MSecond},
    {kind, Kind}, {data_len, DataLen}} = parse_header(Header),
 
-  io:format("~s/~s/~s,~s:~s:~s.~s,~s,~s", [Year, Month, Day, Hour, Minute, Second, MSecond, Kind, DataLen]).
+  io:format("~s/~s/~s,~s:~s:~s.~s,~s,~s",
+            [Year, Month, Day, Hour, Minute, Second, MSecond, Kind, DataLen]).
 
 parse_jnl_header(HeaderBin) ->
   <<Year:4/binary, Month:2/binary, Day:2/binary,
@@ -37,25 +38,59 @@ parse_jnl_header(HeaderBin) ->
      kind = Kind, data_len = DataLen
     }.
 
-print_jnl_header(HeaderRecord) ->
+write_jnl_header(IoDevice, HeaderRecord) ->
   #jnl_header{
      year = Year, month = Month, day = Day,
      hour = Hour, minute = Minute, second = Second, msecond = MSecond,
      kind = Kind, data_len = DataLen} = HeaderRecord,
 
-  io:format("HEADER,~s/~s/~s,~s:~s:~s.~s,~s,~s~n",
+  io:format(IoDevice,
+            "HEADER,~s/~s/~s,~s:~s:~s.~s,~s,~s~n",
             [Year, Month, Day, Hour, Minute, Second, MSecond, Kind, DataLen]).
 
 read_data(HeaderRecord, IoDevice) ->
   DataLen = binary_to_integer(HeaderRecord#jnl_header.data_len),
   file:read(IoDevice, DataLen).
 
-sample_run() ->
-  SampleFile = sample_file(),
-  {ok, IoDevice} = read_open(SampleFile),
+% 表示可能文字を判定する関数
+% この関数をto_printableで利用したいが、以下のエラーとなった。
+% call to local/imported function is_print_byte/1 is illegal in guard
+is_print(Byte) -> (16#21 =< Byte) and (Byte =< 16#7E).
+
+% 表示可能文字でない場合、'.'を返す関数
+to_printable(Byte) ->
+  case is_print(Byte) of
+    true -> Byte;
+    false -> $.
+  end.
+
+% ジャーナルレコードの読込み
+read_record(IoDevice) ->
+  % ジャーナルレコードの読込み
   {ok, HeaderBin} = read_header(IoDevice),
   HeaderRecord = parse_jnl_header(HeaderBin),
-  print_jnl_header(HeaderRecord),
   {ok, DataBin} = read_data(HeaderRecord, IoDevice),
-  file:close(IoDevice),
   {HeaderRecord, DataBin}.
+
+write_jnl_data(IoDevice, DataBin) ->
+  DataList = binary_to_list(DataBin),
+  PrintableList = lists:map(fun to_printable/1, DataList),
+  ok = file:write(IoDevice, "DATA:"),
+  file:write(IoDevice, PrintableList).
+
+write_jnl_record(IoDevice, HeaderRecord, DataBin) ->
+  ok = write_jnl_header(IoDevice, HeaderRecord),
+  write_jnl_data(IoDevice, DataBin).
+
+sample_run() ->
+  SampleFile = sample_file(),
+  {ok, ReadFile} = file:open(SampleFile, [read, binary, raw]),
+  {ok, WriteFile} = file:open("a", [write, binary]),
+
+  {HeaderRecord, DataBin} = read_record(ReadFile),
+
+  ok = write_jnl_record(WriteFile, HeaderRecord, DataBin),
+
+  file:close(ReadFile),
+  file:close(WriteFile),
+  ok.
