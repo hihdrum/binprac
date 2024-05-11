f8cc74be1e2401746aa3f54b38054148f5c8db18
makoto <88492711+hihdrum@users.noreply.github.com>
2024-05-11 13:17:34 +0900

    実装の比較のためにディレクトリを分けることにした。

---
+++ c/i01/jnl.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "jnl.h"
+
+void JnlHeader_Print(const struct jnl_header * const h)
+{
+  printf("%*.*s/%*.*s/%*.*s,%*.*s:%*.*s:%*.*s.%*.*s,%*.*s,%*.*s",
+      JNL_YEAR_LEN, JNL_YEAR_LEN, h->year,
+      JNL_MONTH_LEN, JNL_MONTH_LEN, h->month,
+      JNL_DAY_LEN, JNL_DAY_LEN, h->day,
+      JNL_HOUR_LEN, JNL_HOUR_LEN, h->hour,
+      JNL_MINUTE_LEN, JNL_MINUTE_LEN, h->minute,
+      JNL_SECOND_LEN, JNL_SECOND_LEN, h->second,
+      JNL_MSECOND_LEN, JNL_MSECOND_LEN, h->msecond,
+      JNL_KIND_LEN, JNL_KIND_LEN, h->kind,
+      JNL_DATA_LEN, JNL_DATA_LEN, h->dataLen);
+}
+
+int JnlHeader_DataLen(const struct jnl_header * const h)
+{
+  char dataLenBuf[JNL_DATA_LEN + 1];
+  memcpy(dataLenBuf, h->dataLen, JNL_DATA_LEN);
+  dataLenBuf[JNL_DATA_LEN] = '\0';
+
+  const int dataLen = atoi(dataLenBuf);
+
+  return dataLen;
+}
---
+++ c/i01/jnl_ascii_dump.c
@@ -0,0 +1,121 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include "jnl.h"
+
+void PrintHeader(struct jnl_header * h)
+{
+  printf("HEADER,");
+  JnlHeader_Print(h);
+  putchar('\n');
+}
+
+void ToPrintable(char *pc, int len)
+{
+  for(int i = 0; i < len; i++)
+  {
+    if(!isprint(pc[i]))
+    {
+      pc[i] = '.';
+    }
+  }
+}
+
+const int dataBufferSize = 20 * 1024 * 1024;
+char *dataBuffer;
+
+void dumpStream(FILE *fp)
+{
+  while(1)
+  {
+    struct jnl_header jnlh;
+    size_t retFread = fread(&jnlh, sizeof(jnlh), 1, fp);
+    if(1 != retFread)
+    {
+      if(feof(fp))
+      {
+        break;
+      }
+
+      perror("ジャーナルヘッダfread異常");
+      exit(1);
+    }
+
+    PrintHeader(&jnlh);
+
+    int dataLen = JnlHeader_DataLen(&jnlh);
+
+    retFread = fread(dataBuffer, sizeof(char), dataLen, fp);
+    if(retFread < dataLen)
+    {
+      perror("データfread異常");
+      exit(1);
+    }
+
+    ToPrintable(dataBuffer, dataLen);
+
+    printf("DATA:");
+    int retFwrite = fwrite(dataBuffer, sizeof(char), dataLen, stdout);
+    if(retFwrite != dataLen)
+    {
+      perror("データfwrite異常");
+      exit(1);
+    }
+    putchar('\n');
+  }
+}
+
+void asciiDumpArgs(int argc, char *argv[])
+{
+  for(int i = 1; i < argc; i++)
+  {
+    FILE *fp = fopen(argv[i], "r");
+    if(NULL == fp)
+    {
+      perror("fopen異常");
+      exit(1);
+    }
+
+    dumpStream(fp);
+
+    int retFclose = fclose(fp);
+    if(0 != retFclose)
+    {
+      perror("fclose異常");
+      exit(1);
+    }
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  int isUseStdin = 0;
+  if(argc == 1)
+  {
+    isUseStdin = 1;
+  }
+  else if(argc == 2 && 0 == strcmp("-h", argv[1]))
+  {
+    fprintf(stderr, "Usage : %s ファイル名\n", argv[0]);
+    exit(1);
+  }
+
+  dataBuffer = malloc(dataBufferSize);
+  if(NULL == dataBuffer)
+  {
+    perror("malloc異常");
+    exit(1);
+  }
+
+  if(isUseStdin)
+  {
+    dumpStream(stdin);
+  }
+  else
+  {
+    asciiDumpArgs(argc, argv);
+  }
+
+  return 0;
+}
